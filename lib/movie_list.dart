1f7976a70defa00d8ff8aad6f8da34e7c71f7f7a
bulletBane <tolegena1@gmail.com>
2020-11-09 21:13:41 +0600

    feature image from network

--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -7,6 +7,10 @@ class MovieList extends StatefulWidget {
 }
 
 class _MovieListState extends State<MovieList> {
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+
   String result;
   HttpHelper helper;
   int moviesCount;
@@ -30,6 +34,7 @@ class _MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
+    NetworkImage image;
     return Scaffold(
         appBar: AppBar(
           title: Text('Movies'),
@@ -37,10 +42,18 @@ class _MovieListState extends State<MovieList> {
         body: ListView.builder(
             itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
             itemBuilder: (BuildContext context, int position) {
+              if (movies[position].posterPath != null) {
+                image = NetworkImage(iconBase + movies[position].posterPath);
+              } else {
+                image = NetworkImage(defaultImage);
+              }
               return Card(
                   color: Colors.white,
                   elevation: 2.0,
                   child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: image,
+                    ),
                     title: Text(movies[position].title),
                     subtitle: Text('Released: ' +
                         movies[position].releaseDate +
